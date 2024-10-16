71fe3147e6ae7c00104e82a681ef7f125382e1fa
Nolly Alvarado <147677268+dino1210@users.noreply.github.com>
2024-10-16 11:31:20 +0800

    Added expo go for testing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,11 +21,11 @@ const Login = () => {
                 <div className='flex flex-col items-center mb-5'>
                 <IoPerson className="text-[#17A2B8] h-10 w-auto"/>
                 </div>
-                <label className='text-sm'>Username:</label>
-                <input className='border p-2 w-full bg-[#D9D9D9] rounded-md text-sm' type='text' placeholder='Username' />
-                <label className='text-sm'>Password:</label>
-                <input className='border p-2 w-full bg-[#D9D9D9] rounded-md text-sm mb-5' type='password' placeholder='Password' />   
-                <button className='bg-[#17A2B8] text-white w-full rounded-lg py-1 mb-11' type='submit'>Login</button>
+                <label className='text-xs font-semibold'>Username:</label>
+                <input className='border p-2 w-full bg-[#D9D9D9] rounded-md text-xs' type='text' placeholder='Username' />
+                <label className='text-xs font-semibold'>Password:</label>
+                <input className='border p-2 w-full bg-[#D9D9D9] rounded-md text-xs mb-5' type='password' placeholder='Password' />   
+                <button className='bg-[#17A2B8] text-white font-semibold w-full rounded-lg py-2 mb-11' type='submit'>Login</button>
                 <p className='text-xs text-center bg-[#D9D9D9] -mx-7 p-3.5 rounded-b-[30px]'>Need Help? <a className='text-[#17A2B8]' href="www.google.com" target='_blank'>Contact Administrator</a></p>         
             </form>
         </div>
