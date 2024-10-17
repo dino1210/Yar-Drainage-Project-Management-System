354f1f6300a7aa1f0fc0676776cb840c82f69e2f
Nolly Alvarado <147677268+dino1210@users.noreply.github.com>
2024-10-17 14:24:57 +0800

    testing lol

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const Dasboard = () => {
   return (
     <div className='flex justify-center'>
-      <div className='bg-white rounded-lg mx-5 my-20 w-40 h-40'>
+      <div className='bg-white rounded-lg mx-5 my-20 w-40 h-40 shadow-sm'>
         <h1 className='text-black text-md p-5 '>Ronald J Labrado Jr.</h1>
       </div>
-      <div className='bg-white rounded-lg mx-5 my-20'>
+      <div className='bg-white rounded-lg mx-5 my-20 shadow-md'>
         <h1 className='text-black text-md p-5 '>Ronald J Labrado Jr.</h1>
       </div>
-      <div className='bg-white rounded-lg mx-5 my-20'>
+      <div className='bg-white rounded-lg mx-5 my-20 shadow-lg'>
         <h1 className='text-black text-md p-5 '>Ronald J Labrado Jr.</h1>
       </div>
-      <div className='bg-white rounded-lg mx-5 my-20'>
+      <div className='bg-white rounded-lg mx-5 my-20 shadow-xl'>
         <h1 className='text-black text-md p-5 '>Ronald J Labrado Jr.</h1>
       </div>
-      <div className='bg-white rounded-lg mx-5 my-20'>
+      <div className='bg-white rounded-lg mx-5 my-20 shadow-2xl'>
         <h1 className='text-black text-md p-5 '>Ronald J Labrado Jr.</h1>
       </div>
     </div>
