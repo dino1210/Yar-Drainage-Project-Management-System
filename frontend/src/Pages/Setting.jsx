a2381ce83459efd419f0ef39c68a8939cb8c49bd
Nolly Alvarado <147677268+dino1210@users.noreply.github.com>
2024-10-17 12:57:22 +0800

    .

--- frontend/src/Pages/Setting.jsx
+++ frontend/src/Pages/Setting.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Setting = () => {
   return (
     <div>
-      
+      <h1 className='text-[300px]'>NIGGA</h1>      
     </div>
   )
 }
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,12 +4,12 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 const SIDEBAR_ITEMS = [
-  { name: "Dashboard", icon: BarChart2, color: "white", href: "/",},
-  { name: "Inventory Management", icon: ShoppingBag, color: "white", href: "/inventory" },
-  { name: "Check-In/Check-Out", icon: ListChecks, color: "white", href: "/check" },
-  { name: "Reports", icon: NotepadText, color: "white", href: "/reports" },
-  { name: "User Management", icon: UserRoundCog, color: "white", href: "/user" },
-  { name: "Setting", icon: Settings, color: "white", href: "/setting" },
+  { name: "Dashboard", icon: BarChart2, color: "gray", href: "/",},
+  { name: "Inventory Management", icon: ShoppingBag, color: "gray", href: "/inventory" },
+  { name: "Check-In/Check-Out", icon: ListChecks, color: "gray", href: "/check" },
+  { name: "Reports", icon: NotepadText, color: "gray", href: "/reports" },
+  { name: "User Management", icon: UserRoundCog, color: "gray", href: "/user" },
+  { name: "Setting", icon: Settings, color: "gray", href: "/setting" },
 ];
 
 
@@ -19,31 +19,31 @@ const Sidebar = () => {
     <motion.div
       className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
         isSidebarOpen ? "w-64" : "w-20"
-      }`}
+      } shadow-lg`}
       animate={{ width: isSidebarOpen ? 256 : 80 }}
     >
-      <div className="h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700">
+      <div className="h-full bg-white p-4 flex flex-col border-r">
         <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          className="p-2 mb-20 rounded-full hover:bg-gray-700 transition-colors max-w-fit"
+          className="p-2 mb-16 rounded-full hover:bg-gray-700 transition-colors max-w-fit"
         >
-          <Menu size={24} />
+          <Menu size={24} style={{ color: "gray" }}/>
         </motion.button>
 
         <nav className="mt-5 flex-grow">
           {SIDEBAR_ITEMS.map((item) => (
             <Link key={item.href} to={item.href}>
-              <motion.div className="flex items-center p-2 text-sm font-medium rounded-lg hover:bg-gray-900 transition-colors mb-4">
-                <item.icon
-                  size={22}
-                  style={{ color: item.color, minWidth: "22px" }}
-                />
+                <motion.div className="flex items-center p-2 text-sm font-medium rounded-lg hover:bg-gray-900 transition-colors mb-4">
+                  <item.icon
+                    size={22}
+                    style={{ color: item.color, minWidth: "22px" }}
+                  />
                 <AnimatePresence>
                   {isSidebarOpen && (
                     <motion.span
-                      className="ml-4 whitespace-nowrap"
+                      className="ml-4 whitespace-nowrap text-gray-800"
                       initial={{ opacity: 0, width: 0 }}
                       animate={{ opacity: 1, width: "auto" }}
                       exit={{ opacity: 0, width: 0 }}
